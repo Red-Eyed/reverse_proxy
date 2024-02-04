a7574f9c8db665985adbb03d94a4a848c441c21f
Vadym Stupakov <vadim.stupakov@gmail.com>
2024-02-04 11:46:23 +0200

    Initial commit

---
+++ reverse_proxy/generate_nginx_conf.py
@@ -0,0 +1,41 @@
+path2url = {
+    "/netron": "http://localhost:8080",
+    "/anotherpath": "http://localhost:9090",
+    # Add more path mappings as needed
+}
+
+nginx_conf_template = """
+server {{
+    listen 80;
+
+{locations}
+
+    location / {{
+        return 404;
+    }}
+}}
+"""
+
+location_block_template = """
+    location {path} {{
+        proxy_pass {url};
+        include /config/nginx/proxy.conf;  # Includes common proxy settings
+    }}
+"""
+
+def generate_location_blocks(path2url):
+    location_blocks = ""
+    for path, url in path2url.items():
+        location_blocks += location_block_template.format(path=path, url=url)
+    return location_blocks
+
+def generate_nginx_conf(path2url, filename="./config/default.conf"):
+    locations = generate_location_blocks(path2url)
+    nginx_conf = nginx_conf_template.format(locations=locations)
+
+    with open(filename, "w") as file:
+        file.write(nginx_conf)
+    print(f"NGINX configuration written to {filename}")
+
+if __name__ == "__main__":
+    generate_nginx_conf(path2url)
