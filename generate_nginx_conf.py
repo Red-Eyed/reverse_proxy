46a3cb40abb70b14fc81d6c418b04d41eddc5446
Vadym Stupakov <vadim.stupakov@gmail.com>
2024-02-11 11:20:55 +0200

    wip

--- generate_nginx_conf.py
+++ generate_nginx_conf.py
@@ -13,13 +13,44 @@ server {{
         return 404;
     }}
 }}
+
+map $http_upgrade $connection_upgrade {{
+    # If the Upgrade header is present, set Connection to "upgrade"
+    default upgrade;
+
+    # If the Upgrade header is not present, set Connection to "close"
+    '' close;
+}}
 """
 
 location_block_template = """
     location {path} {{
-        rewrite ^{path}(/.*)$ $1 break;
+        # Proxy the request to the backend server running on a specific port
         proxy_pass {url};
-        include /config/nginx/proxy.conf;  # Includes common proxy settings
+
+        # Use HTTP/1.1 for the connection to the backend server
+        proxy_http_version 1.1;
+
+        # Pass the Upgrade header from the client to the backend server, necessary for WebSocket connections
+        proxy_set_header Upgrade $http_upgrade;
+
+        # Use the value of $connection_upgrade for the Connection header, determined by the map block
+        proxy_set_header Connection $connection_upgrade;
+
+        # Pass the original Host header to the backend server
+        proxy_set_header Host $host;
+
+        # Pass the client IP address to the backend server, useful for logging and analytics
+        proxy_set_header X-Real-IP $remote_addr;
+
+        # Pass the original request's IP addresses (client, proxies) to the backend server
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        # Pass the scheme (http or https) used by the client to the backend server
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        # Turn off buffering to stream responses directly to the client, beneficial for WebSockets and streaming responses
+        proxy_buffering off;
     }}
 """
 
